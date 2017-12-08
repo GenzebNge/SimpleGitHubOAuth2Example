2010e194ef857134959baea302f1f3ac655c1f9d
aoa4eva <afua.ankomah@gmail.com>
2017-12-08 16:43:46 -0500

    Modified configuration - added Google Account

--- src/main/java/me/afua/githuboauthexample/MainController.java
+++ src/main/java/me/afua/githuboauthexample/MainController.java
@@ -2,8 +2,6 @@ package me.afua.githuboauthexample;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
